e006a6e257cc8d8fbe3db6b68054eab35a8d38d4
mororong9 <kangmoonju0603@gmail.com>
2022-07-29 17:51:41 +0900

    220729 algorithm test

--- 1회차/강문주/1_소득불균형.py
+++ 1회차/강문주/1_소득불균형.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_소득불균형.txt")
+
+T = int(input())
+income = []
+total = 0
+
+for test_case in range(1, T + 1):
+    num = int(input())
+    income = list(map(int, input().split()))
+    income_avg = sum(income) / num
+    cnt=0
+    for c in income:
+        if c <= income_avg:
+            cnt += 1
+    print(f'#{test_case} {cnt}')
